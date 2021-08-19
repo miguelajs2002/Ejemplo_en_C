810b048dc9a323d53795a018d0749abaf23bce69
miguelajs2002 <87503487+miguelajs2002@users.noreply.github.com>
2021-08-19 16:52:39 -0500

    Prueba
    Prueba

---
+++ main.c
@@ -0,0 +1,20 @@
+/*nombre ddel programa
+Autor MOdificador por
+que hace el programa
+necesita el circuito
+colocar una imagen en el repositorio_ Liga
+terminales que se utilizan, el modo
+ejemplo: gpio 5 salida
+*/
+
+/*Bibliotecas */
+#include <stdio.h>
+
+/*declariacion de variables*/
+
+/*declaracion main */
+
+int main(){
+    printf("Hola UAM");
+    return 0;
+}
