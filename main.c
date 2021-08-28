7e1f4761d999cf2f53c2b062132ce03936a82433
miguelajs2002 <87503487+miguelajs2002@users.noreply.github.com>
2021-08-28 04:35:02 -0500

    PROGRAMACION BASICA

--- main.c
+++ main.c
@@ -1,20 +1,56 @@
-/*nombre ddel programa
-Autor MOdificador por
-que hace el programa
-necesita el circuito
-colocar una imagen en el repositorio_ Liga
-terminales que se utilizan, el modo
-ejemplo: gpio 5 salida
-*/
-
-/*Bibliotecas */
 #include <stdio.h>
 
-/*declariacion de variables*/
+int main () {
+    int opcion = 1;
+    float var1, var2;
+    
+    while(opcion != 0){
+        
+        printf("\n0. Salir \n");
+        printf("1. Suma \n");
+        printf("2. Resta \n");
+        printf("3. Multiplicacion \n");
+        printf("4. Division \n\n");
 
-/*declaracion main */
+        printf("Selecciona una opcion: ");
+        scanf("%d",&opcion);
 
-int main(){
-    printf("Hola UAM");
-    return 0;
+        if(opcion == 0){    
+        } else if(opcion == 1){
+            printf("Ha seleccionado la opcion %d. Suma\n", opcion);
+            printf("Introduce los datos\n");
+            printf("A = ");
+            scanf("%f", &var1);
+            printf("B = ");
+            scanf("%f", &var2);
+            printf("A + B = %.4f\n",(var1 + var2));
+        } else if (opcion == 2){
+            printf("Has seleccionado la opcion %d. Resta\n", opcion);
+            printf("Introduce los datos\n");
+            printf("A = ");
+            scanf("%f", &var1);
+            printf("B = ");
+            scanf("%f", &var2);
+            printf("A - B = %.4f\n",(var1 - var2));
+        } else if (opcion == 3){
+            printf("Has seleccionado la opcion %d. Multiplicacion\n", opcion);
+            printf("Introduce los datos\n");
+            printf("A = ");
+            scanf("%f", &var1);
+            printf("B = ");
+            scanf("%f", &var2);
+            printf("A * B = %.4f\n",(var1 * var2));
+        } else if (opcion == 4){
+            printf("Has seleccionado la opcion %d. Division\n", opcion);
+            printf("Introduce los datos\n");
+            printf("A = ");
+            scanf("%f", &var1);
+            printf("B = ");
+            scanf("%f", &var2);
+            printf("A / B = %.4f\n",(var1 / var2));
+        } else {
+            printf("----Escoja una opcion valida----\n");
+        }
+    } 
+
 }
